599d046c78b076df6d1829ce27694a0d5133aa34
patrick11514 <ja@patrick115.eu>
2022-12-16 01:44:10 +0100

    Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['public/**/*.html'],
+    theme: {
+        extend: {},
+        fontFamily: {
+            roboto: ['Roboto', 'sans-serif'],
+        },
+    },
+    plugins: [],
+}
