72dc93494fce8e0806fe3e6a6f59715e80035edf
patrick11514 <ja@patrick115.eu>
2023-03-06 10:20:35 +0100

    Added admin page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import { data } from '$lib/data.svelte'
     import Login from './login.svelte'
+    import Admin from './admin.svelte'
 </script>
 
 Username: {$data?.username}
 
 {#if $data?.logged}
-    Jsi lognuty LOL XD
+    <Admin />
 {:else}
     <Login />
 {/if}
