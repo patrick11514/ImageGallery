64b35b4d937dc78443e0c2467517a55f26f17992
patrick11514 <ja@patrick115.eu>
2023-03-16 23:00:39 +0100

    Added title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,12 @@
     import Redirect from '../components/redirect.svelte'
 </script>
 
+<svelte:head>
+    <title>Login | Patrik Mintěl</title>
+</svelte:head>
+
 {#if $data?.logged}
-    <Redirect link="/admin" />
+    <Redirect link="/admin/home" />
 {:else}
     <Login />
 {/if}
