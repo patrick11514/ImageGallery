56eb1fb5fee639093b563227deeb4495c5c44177
patrick11514 <ja@patrick115.eu>
2023-03-21 10:30:47 +0100

    Stats page

---
+++ src/components/content/stats.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import type { StatsResponse } from '$types/response'
+    import type { Stat } from '$types/types'
+    import { onMount } from 'svelte'
+
+    let data: Stat[]
+
+    onMount(async () => {
+        let data = await fetch('/api/images/stats')
+        let json = (await data.json()) as StatsResponse
+        if (json.status) {
+        }
+    })
+</script>
+
+{#if !data}
+    <div class="w-full h-full">
+        <img src="/static/loading.gif" alt="Loading" />
+    </div>
+{:else}
+    <div class="grid grid-cols-5" />
+{/if}
