22443786d62798e20b8d2e126f0803dfe3e19854
patrick11514 <ja@patrick115.eu>
2023-03-21 22:09:49 +0100

    Loading gif + working on statuses

--- src/components/content/stats.svelte
+++ src/components/content/stats.svelte
@@ -2,21 +2,25 @@
     import type { StatsResponse } from '$types/response'
     import type { Stat } from '$types/types'
     import { onMount } from 'svelte'
+    import Loading from '../loading.svelte'
 
     let data: Stat[]
 
     onMount(async () => {
-        let data = await fetch('/api/images/stats')
-        let json = (await data.json()) as StatsResponse
+        let request = await fetch('/api/images/stats')
+        let json = (await request.json()) as StatsResponse
         if (json.status) {
+            data = json.data
         }
     })
 </script>
 
 {#if !data}
-    <div class="w-full h-full">
-        <img src="/static/loading.gif" alt="Loading" />
-    </div>
+    <Loading />
 {:else}
-    <div class="grid grid-cols-5" />
+    <div class="grid grid-cols-5">
+        {#each data as status}
+            <div style={`background-color: ${status.color};`}>{status.name}</div>
+        {/each}
+    </div>
 {/if}
---
+++ src/components/loading.svelte
@@ -0,0 +1,8 @@
+<div class="w-full h-full flex flex-col">
+    <img
+        class="max-w-max max-h-max w-20 h-auto mx-auto mt-auto mb-0"
+        src="/loading.gif"
+        alt="Loading"
+    />
+    <h1 class="mx-auto mb-auto mt-0 font-bold text-2xl">Načítání</h1>
+</div>
